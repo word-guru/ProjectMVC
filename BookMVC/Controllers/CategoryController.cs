42ac07582be535822864ada1c770d350eecf1e92
Pavel P <legeon48@mail.ru>
2023-01-14 12:45:28 +0300

    update

--- BookMVC/Controllers/CategoryController.cs
+++ BookMVC/Controllers/CategoryController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookMVC.Data;
+using BookMVC.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookMVC.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
