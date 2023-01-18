a6e25655c8883058f58494c30088d2f54726739a
Pavel P <legeon48@mail.ru>
2023-01-18 15:06:46 +0300

    Added Edit

--- BookMVC/Controllers/CategoryController.cs
+++ BookMVC/Controllers/CategoryController.cs
@@ -40,5 +40,39 @@ namespace BookMVC.Controllers
             }
             return View(obj);
         }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _db.Categories.Find(id);
+            //var categoryFromDbFirst = _db.Categories.FirstOrDefault(x => x.Id == id);
+            //var categoryFromDbSingl = _db.Categories.SingleOrDefault(x => x.Id == id);
+            if(categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
     }
 }
--- BookMVC/Models/Category.cs
+++ BookMVC/Models/Category.cs
@@ -10,6 +10,7 @@ namespace BookMVC.Models
         [Required]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display Order must be between 1 and 100 only !!!")]
         public int DisplayOrder { get; set; }
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
     }
